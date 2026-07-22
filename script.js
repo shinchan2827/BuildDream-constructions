4f4633dcd914beb263b1c8a910db2199666e7056
shinchan2827 <vaishuvaishnaviacs@gmail.com>
2026-07-22 14:11:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const projects = [
+  {
+    image: "images/9723510-waterfront-luxury-villa-in-turks-.jpg",
+    title: "Luxury Villa",
+    desc: "Modern luxury villa with premium design and construction."
+  },
+  {
+    image: "images/4952584.jpg",
+    title: "Commercial Building",
+    desc: "Beautiful modern commercial spaces built for efficiency, innovation, and success."
+  },
+  {
+    image: "images/3671451.jpg",
+    title: "Bridge",
+    desc: "A modern bridge designed for strength, safety, and long-term reliability."
+  },
+];
+
+let current = 0;
+
+const projectImage = document.getElementById("projectImage");
+const projectTitle = document.getElementById("projectTitle");
+const projectDesc = document.getElementById("projectDesc");
+
+document.getElementById("next").addEventListener("click", () => {
+  current = (current + 1) % projects.length;
+  updateProject();
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+  current = (current - 1 + projects.length) % projects.length;
+  updateProject();
+});
+
+function updateProject() {
+  projectImage.src = projects[current].image;
+  projectTitle.textContent = projects[current].title;
+  projectDesc.textContent = projects[current].desc;
+}
